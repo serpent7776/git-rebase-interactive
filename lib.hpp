638c91945431cbaf7e908dda01d884b7b0a09841
Serpent7776 <serpent7776@gmail.com>
2022-03-03 21:39:38 +0100

    Added split function

--- lib.hpp
+++ lib.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
 #include <string_view>
+#include <utility>
+#include <tuple>
 
 namespace str
 {
@@ -19,6 +22,18 @@ std::string join(char sep, String&& head, Strings&& ...rest)
 	std::string result = to_string(head);
 	(result.append(sep + to_string(rest)), ...);
 	return result;
+}
+
+inline std::tuple<bool, std::string_view, std::string_view> split(char sep, std::string_view str)
+{
+	const auto first = std::begin(str);
+	const auto last = std::end(str);
+	const auto middle = std::find(first, last, sep);
+	if (middle == std::end(str))
+	{
+		return {false, {}, {}};
+	}
+	return std::tuple(true, std::string_view(first, middle), std::string_view(middle + 1, last));
 }
 
 }
