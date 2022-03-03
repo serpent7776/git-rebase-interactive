d708f324cbaa8de7828a018a34dd095045f2094a
Serpent7776 <serpent7776@gmail.com>
2022-03-03 21:39:21 +0100

    Added tests for split function

--- tests.cpp
+++ tests.cpp
@@ -13,3 +13,51 @@ TEST_CASE("variadic join works for 3 strings")
 	const auto result = str::join(':', "foo", "bar", "baz");
 	REQUIRE(result == "foo:bar:baz");
 }
+
+TEST_CASE("split splits on separator")
+{
+	const auto [ok, left, right] = str::split(':', "foo:bar");
+	REQUIRE(ok);
+	REQUIRE(left == "foo");
+	REQUIRE(right == "bar");
+}
+
+TEST_CASE("split string with no right part")
+{
+	const auto [ok, left, right] = str::split(':', "1234:");
+	REQUIRE(ok);
+	REQUIRE(left == "1234");
+	REQUIRE(right == "");
+}
+
+TEST_CASE("split string with no left part")
+{
+	const auto [ok, left, right] = str::split(':', ":7890");
+	REQUIRE(ok);
+	REQUIRE(left == "");
+	REQUIRE(right == "7890");
+}
+
+TEST_CASE("split string with neither left nor right part")
+{
+	const auto [ok, left, right] = str::split(':', ":");
+	REQUIRE(ok);
+	REQUIRE(left == "");
+	REQUIRE(right == "");
+}
+
+TEST_CASE("split string that does not contain separator")
+{
+	const auto [ok, left, right] = str::split(',', "foobar");
+	REQUIRE(not ok);
+	REQUIRE(left == "");
+	REQUIRE(right == "");
+}
+
+TEST_CASE("split an empty string")
+{
+	const auto [ok, left, right] = str::split(',', "");
+	REQUIRE(not ok);
+	REQUIRE(left == "");
+	REQUIRE(right == "");
+}
